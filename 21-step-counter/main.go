9794f641b825a78e467e3b95b1e618b939f9f676
Gabriel Fu <hfu.gabriel@gmail.com>
2023-12-22 11:33:21 +0800

    Impl: day 21 part 2

--- 21-step-counter/main.go
+++ 21-step-counter/main.go
@@ -32,6 +32,18 @@ func (g *Grid) String() string {
 	return strings.Join(g.data, "\n")
 }
 
+func (g *Grid) Round(coord Coord) Coord {
+	r := coord.r % g.h
+	for r < 0 {
+		r += g.h
+	}
+	c := coord.c % g.w
+	for c < 0 {
+		c += g.w
+	}
+	return Coord{r, c}
+}
+
 func (g *Grid) Get(coord Coord) byte {
 	return g.data[coord.r][coord.c]
 }
@@ -72,36 +84,24 @@ type Entry struct {
 	steps int
 }
 
-func traverse(g *Grid, start Coord, steps int) [][]VisitStatus {
-	visited := make([][]VisitStatus, g.h)
-	for i := range visited {
-		visited[i] = make([]VisitStatus, g.w)
-	}
-
+func traverse(g *Grid, start Coord, steps int) map[int]int {
+	res := make(map[int]int)
+	visited := make(map[Coord]struct{})
 	queue := []Entry{{start, 0}}
 	var entry Entry
 	for len(queue) > 0 {
 		entry, queue = queue[0], queue[1:]
 
-		if visited[entry.coord.r][entry.coord.c] != Unvisited {
+		if entry.steps == steps+1 {
 			continue
 		}
 
-		if g.Get(entry.coord) == '#' {
-			visited[entry.coord.r][entry.coord.c] = Unreachable
+		if _, ok := visited[entry.coord]; ok {
 			continue
 		}
 
-		switch entry.steps % 2 {
-		case 0:
-			visited[entry.coord.r][entry.coord.c] = Even
-		case 1:
-			visited[entry.coord.r][entry.coord.c] = Odd
-		}
-
-		if entry.steps == steps {
-			continue
-		}
+		res[entry.steps]++
+		visited[entry.coord] = struct{}{}
 
 		for _, next := range []Coord{
 			{entry.coord.r - 1, entry.coord.c},
@@ -109,18 +109,12 @@ func traverse(g *Grid, start Coord, steps int) [][]VisitStatus {
 			{entry.coord.r, entry.coord.c - 1},
 			{entry.coord.r, entry.coord.c + 1},
 		} {
-			if next.r < 0 || next.r >= g.h || next.c < 0 || next.c >= g.w {
-				continue
+			if g.Get(g.Round(next)) != '#' {
+				queue = append(queue, Entry{next, entry.steps + 1})
 			}
-
-			if visited[next.r][next.c] != Unvisited {
-				continue
-			}
-
-			queue = append(queue, Entry{next, entry.steps + 1})
 		}
 	}
-	return visited
+	return res
 }
 
 func Count(visited [][]VisitStatus, status VisitStatus) int {
@@ -135,15 +129,43 @@ func Count(visited [][]VisitStatus, status VisitStatus) int {
 	return total
 }
 
+func Solve(grid *Grid, n int) int {
+	res := traverse(grid, grid.StartPos(), n)
+	total := 0
+	for dist, num := range res {
+		if dist%2 == n%2 {
+			total += num
+		}
+	}
+	return total
+}
+
 func part1() {
 	lines := ReadLines("input.txt")
 	grid := NewGrid(lines)
-	visited := traverse(grid, grid.StartPos(), 64)
-	total := Count(visited, Even)
+	total := Solve(grid, 64)
 	fmt.Println(total)
 }
 
 func part2() {
+	lines := ReadLines("input.txt")
+	grid := NewGrid(lines)
+
+	size := grid.h
+	half := size / 2
+
+	y0 := Solve(grid, half)
+	y1 := Solve(grid, half+size)
+	y2 := Solve(grid, half+size*2)
+
+	// solve polynomial
+	a := (y2 - 2*y1 + y0) / 2
+	b := (y1 - y0) - a
+	c := y0
+
+	target := (26501365 - half) / size
+	total := a*target*target + b*target + c
+	fmt.Println(total)
 }
 
 func main() {

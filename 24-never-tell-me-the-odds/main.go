8ac0a279822a8a62d6e573043ba28ecd4d00f2fd
Gabriel Fu <hfu.gabriel@gmail.com>
2024-01-05 16:31:59 +0800

    Impl: day 24 part 2

--- 24-never-tell-me-the-odds/main.go
+++ 24-never-tell-me-the-odds/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"strings"
 	"time"
@@ -93,11 +94,6 @@ func part1() {
 			li := lines[i]
 			lj := lines[j]
 			intersect, ok := Intersect2(li, lj)
-			// if ok {
-			// 	fmt.Printf("%s intersects %s at %s\n", li, lj, intersect)
-			// } else {
-			// 	fmt.Printf("%s does not intersect %s\n", li, lj)
-			// }
 
 			if ok &&
 				li.PointIsFuture(intersect) &&
@@ -110,7 +106,84 @@ func part1() {
 	fmt.Println(total)
 }
 
+func GaussianElimination(matrix [][]float64) []float64 {
+	nr, nc := len(matrix), len(matrix[0])
+	r, c := 0, 0
+	for r < nr && c < nc {
+		max_ := 0.0
+		imax := -1
+		for i := r; i < nr; i++ {
+			cand := math.Abs(matrix[i][c])
+			if cand > max_ {
+				max_ = cand
+				imax = i
+			}
+		}
+		if matrix[imax][c] == 0 {
+			c++
+		} else {
+			matrix[r], matrix[imax] = matrix[imax], matrix[r]
+			for i := r + 1; i < nr; i++ {
+				factor := matrix[i][c] / matrix[r][c]
+				matrix[i][c] = 0
+				for j := c + 1; j < nc; j++ {
+					matrix[i][j] -= factor * matrix[r][j]
+				}
+			}
+		}
+		r++
+		c++
+	}
+
+	result := make([]float64, nr)
+	for i := nr - 1; i >= 0; i-- {
+		for j := i + 1; j < nc-1; j++ {
+			matrix[i][nc-1] -= matrix[i][j] * result[j]
+		}
+		result[i] = matrix[i][nc-1] / matrix[i][i]
+	}
+	return result
+}
+
+func CrossProduct(a Coord, b Coord) Coord {
+	return Coord{
+		a.y*b.z - a.z*b.y,
+		a.z*b.x - a.x*b.z,
+		a.x*b.y - a.y*b.x,
+	}
+}
+
+func Diff(a Coord, b Coord) Coord {
+	return Coord{
+		a.x - b.x,
+		a.y - b.y,
+		a.z - b.z,
+	}
+}
+
 func part2() {
+	content := ReadFile("input.txt")
+
+	// Px Py Pz Vx Vy Vz t1 t2 t3
+	p0, v0 := ParseFileLine(content[0])
+	p1, v1 := ParseFileLine(content[1])
+	p2, v2 := ParseFileLine(content[2])
+
+	c0 := CrossProduct(p0, v0)
+	c1 := CrossProduct(p1, v1)
+	c2 := CrossProduct(p2, v2)
+
+	matrix := [][]float64{
+		{+(v0.y - v1.y), -(v0.x - v1.x), 0, -(p0.y - p1.y), +(p0.x - p1.x), 0, Diff(c0, c1).z},
+		{-(v0.z - v1.z), 0, +(v0.x - v1.x), +(p0.z - p1.z), 0, -(p0.x - p1.x), Diff(c0, c1).y},
+		{0, +(v0.z - v1.z), -(v0.y - v1.y), 0, -(p0.z - p1.z), +(p0.y - p1.y), Diff(c0, c1).x},
+		{+(v1.y - v2.y), -(v1.x - v2.x), 0, -(p1.y - p2.y), +(p1.x - p2.x), 0, Diff(c1, c2).z},
+		{-(v1.z - v2.z), 0, +(v1.x - v2.x), +(p1.z - p2.z), 0, -(p1.x - p2.x), Diff(c1, c2).y},
+		{0, +(v1.z - v2.z), -(v1.y - v2.y), 0, -(p1.z - p2.z), +(p1.y - p2.y), Diff(c1, c2).x},
+	}
+
+	result := GaussianElimination(matrix)
+	fmt.Println(int(math.Round(result[0] + result[1] + result[2])))
 }
 
 func main() {
